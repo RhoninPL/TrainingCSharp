f059acd0b5017c7f61b440b8fda164b3690680a4
Michal Switalik <michal.switalik@gmail.com>
2015-12-23 11:38:35 +0100

    some XML parser

---
+++ SqlTestConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+namespace Training
+{
+    public class SqlTestConnection
+    {
+        public SqlTestConnection(string user, string password,string dbType, string dbName)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "user id=" + user + ";" +
+            "password=" + password +
+           // "Trusted_Connection=no;" +
+            "database=" + dbName;
+            connection.Open();
+        }
+    }
+}
