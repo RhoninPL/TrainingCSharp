feb7211f2bfaa56ca22ef8fb16596fc92b1db21d
Michal Switalik <michal.switalik@gmail.com>
2016-01-18 18:25:30 +0100

    mvc begin

---
+++ MyPage/Areas/Admin/Controllers/ContentController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPage.Models;
+
+namespace MyPage.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    [RoutePrefix("Content")]
+    public class ContentController : Controller
+    {
+        // GET: Admin/Content
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ContentModels model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // todo update
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        public ActionResult Edit(ContentModels model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // todo update
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyPage/Models/ContentModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MyPage.Models
+{
+    public class ContentModels
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        
+        public DateTime? Create { get; set; }
+        public DateTime? LastModified { get; set; }
+
+        [Required]
+        public string Visible { get; set; }
+        [Required]
+        public int Level { get; set; }
+        /*
+        `id` INT NOT NULL AUTO_INCREMENT,
+  `content` TEXT NULL,
+  `create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastmodified` DATETIME NULL ON UPDATE CURRENT_TIMESTAMP,
+  `visibe` VARCHAR(45) NOT NULL,
+  `level` INT NULL,
+        */
+    }
+}
