8d2b3625b31f38b4b22bf10f11558771f2d5cf4d
Michał Świtalik <michal.switalik@gmail.com>
2016-06-13 09:14:24 +0200

    Switch statement test

---
+++ Training/Statement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    public class Statement
+    {
+        public static void Write(string option)
+        {
+            switch (option)
+            {
+                case "Pierwsza":
+                    Console.WriteLine("Pierwsza linia");
+                    goto case "Druga";
+                case "Druga":
+                    Console.WriteLine("Pierwsza linia");
+                    break;
+
+                default:
+                    Console.WriteLine("Default opcja");
+                    break;
+            }
+        }
+    }
+}
